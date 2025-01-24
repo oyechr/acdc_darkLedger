dd30c78c6999ff98fd84dda3fdfc35a4ce7c1a93
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2025-01-24 10:37:52 +0100

    Add styled Google maps

--- darkledger/src/components/DarkLedger.tsx
+++ darkledger/src/components/DarkLedger.tsx
@@ -1,14 +1,34 @@
 import React, { FC } from 'react'
 import './DarkLedger.css'
 import { IDarkLedgerProps } from './types'
+import { AdvancedMarker, APIProvider, Map, MapCameraChangedEvent, Marker, Pin } from '@vis.gl/react-google-maps'
 
 export const DarkLedger: FC<IDarkLedgerProps> = (props) => {
+  console.log(props)
+  const position = { lat: 59.9762408, lng: 10.6570546 }
+
+  const CustomizedMarker = () => (
+    <AdvancedMarker position={position}>
+      <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+    </AdvancedMarker>
+  )
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>DARK LEDGER</p>
-      </header>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <APIProvider apiKey={'AIzaSyCVob_fs-zku-rc9G7R1dBYk82LXe0IbpI'} onLoad={() => console.log('Maps API has loaded.')}>
+        <Map
+          defaultZoom={13}
+          mapId='ba44130e3947cd5c'
+          defaultCenter={position}
+          streetViewControl={false}
+          mapTypeControl={false}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+          }
+        >
+          <CustomizedMarker />
+        </Map>
+      </APIProvider>
     </div>
   )
 }
